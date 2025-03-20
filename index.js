26423da157a85ef1943fb9e0d792891d33272848
Fedor Indutny <238531+indutny@users.noreply.github.com>
2025-03-19 20:07:23 -0700

    chore: prebuild

--- index.js
+++ index.js
@@ -12,7 +12,19 @@ exports.isSupported = false;
 exports.Stream = FallbackStream;
 
 try {
-  const { Stream } = require('bindings')('mac-screen-share');
+  const { Stream } = require('bindings')({
+    bindings: 'mac-screen-share',
+    try: [
+      [
+        'module_root',
+        'prebuilds',
+        `${process.platform}-${process.arch}`,
+        '@indutny+mac-screen-share.node',
+      ],
+      ['module_root', 'build', 'Release', 'bindings'],
+      ['module_root', 'build', 'Debug', 'bindings'],
+    ],
+  });
 
   if (Stream) {
     exports.Stream = Stream;
